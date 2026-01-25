6bf1a7fa222049f7018d600f1a666c5d62a0a91d
Mateusz Bagiński <bagginsmatthew@gmail.com>
2026-01-25 11:23:53 +0100

    REPO_PATH

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,10 @@
 import pathlib
 
-DEFAULT_BLOCKED_APPS_PATH = (
-    pathlib.Path(__file__).parent.parent / "default_blocked_apps.json"
-)
-BLOCKED_APPS_PATH = pathlib.Path(__file__).parent.parent / "blocked_apps.json"
-LOGS_DIR = pathlib.Path(__file__).parent.parent / "logs"
+REPO_PATH = pathlib.Path(__file__).parent.parent
+
+DEFAULT_BLOCKED_APPS_PATH = REPO_PATH / "default_blocked_apps.json"
+BLOCKED_APPS_PATH = REPO_PATH / "blocked_apps.json"
+LOGS_DIR = REPO_PATH / "logs"
 
 BLOCKED_APPS_CHECK_INTERVAL: float = 1.0  # 1 second
 BLOCKED_APPS_RESET_INTERVAL: float = 2 * 60.0  # 2 minutes
